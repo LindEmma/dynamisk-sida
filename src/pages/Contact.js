088f9b138bd906b341bc5677f69a5262bfe488ab
Emma <elune.lind@gmail.com>
2024-02-20 15:15:53 +0100

    .

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,8 @@
-import Form from "../components/Form";
-
 export default function Contact() {
   return (
     <div className="contact-form">
       <h1>Kontakt</h1>
       <p>Vänligen fyll i uppgifterna nedan och skicka så återkommer jag!</p>
-      <Form></Form>
-      <script
-        type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"
-      ></script>
     </div>
   );
 }
